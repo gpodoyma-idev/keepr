4fe780b0941efce4a56ec02b3f948ceeaaa99717
Григорий Подойма <grigory.podoyma@ingate.ru>
2014-02-14 14:52:44 +0400

    Сендбокс.

---
+++ Api/Sandbox/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sandbox {
+    class Program {
+        //note: как обычно. под страхом сеппуку не коммитим этот файл. / Подойма /
+        static void Main(string[] args) {
+            var start = DateTime.Now;
+            try {
+                Console.WriteLine("Keeper sandbox start.");
+                //note: пишем здесь.
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("Keeper sandbox end. Time elapsed: {0}", (DateTime.Now - start));
+            Console.ReadLine();
+        }
+    }
+}
